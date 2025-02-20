ff1fba739ab8e241886b1eb3c6dd20fde57d41e9
Brandon <53587182+TWB0109@users.noreply.github.com>
2025-02-20 00:13:20 -0600

    animation, indentation and scroll

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -7,17 +7,24 @@ return {
     -- your configuration comes here
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
+    animate = {
+      enabled = true,
+      duartion = 30,
+      easing = 'linear',
+      fps = 75,
+    },
     bigfile = { enabled = false },
     dashboard = { enabled = false },
+    dim = { enabled = true },
     explorer = { enabled = false },
     image = { enabled = true },
-    indent = { enabled = false },
+    indent = { enabled = true },
     input = { enabled = false },
     notifier = { enabled = true },
     picker = { enabled = false },
     quickfile = { enabled = false },
     scope = { enabled = false },
-    scroll = { enabled = true },
+    scroll = { enabled = false },
     statuscolumn = { enabled = false },
     words = { enabled = false },
   },
