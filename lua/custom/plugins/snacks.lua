36fb088d29a0806fbc7087af880633cf1f11e97b
Brandon <53587182+TWB0109@users.noreply.github.com>
2025-02-19 23:56:47 -0600

    fell for the snacks

---
+++ lua/custom/plugins/snacks.lua
@@ -0,0 +1,24 @@
+return {
+  'folke/snacks.nvim',
+  priority = 1000,
+  lazy = false,
+  ---@type snacks.Config
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+    bigfile = { enabled = false },
+    dashboard = { enabled = false },
+    explorer = { enabled = false },
+    image = { enabled = true },
+    indent = { enabled = false },
+    input = { enabled = false },
+    notifier = { enabled = true },
+    picker = { enabled = false },
+    quickfile = { enabled = false },
+    scope = { enabled = false },
+    scroll = { enabled = true },
+    statuscolumn = { enabled = false },
+    words = { enabled = false },
+  },
+}
